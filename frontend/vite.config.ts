b32f3e32b219136126fd057936660c29c0834eaa
mohammed-alaa40123 <mohammedalaa40123@gmail.com>
2024-12-02 23:36:50 -0500

    dott

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: "./ECE461-group32/",
+  base: "/ECE461-group32/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
