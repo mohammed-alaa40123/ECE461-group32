d344299869fd732b11104c5ab9dc02763958b97d
mohammed-alaa40123 <mohammedalaa40123@gmail.com>
2024-12-03 00:02:22 -0500

    f

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
