a16fb67141e9ef1e591baabf38873870226949a8
mohammed-alaa40123 <mohammedalaa40123@gmail.com>
2024-12-04 23:45:31 -0500

    *

--- backend/src/utils/response.ts
+++ backend/src/utils/response.ts
@@ -7,7 +7,7 @@ export const sendResponse = (statusCode: number, body: any) => {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, X-Authorization, X-Amz-Date, X-Api-Key, X-Amz-Security-Token',
+        'Access-Control-Allow-Headers': '*',
       },
       body: JSON.stringify(body),
     };
