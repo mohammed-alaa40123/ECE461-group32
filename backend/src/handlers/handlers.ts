16a568e665491d61ce3983af0829c5c38ef88a66
zaradisht2003 <bola.yosry@gmail.com>
2024-12-05 16:23:20 -0500

    added authentication to update

--- backend/src/handlers/handlers.ts
+++ backend/src/handlers/handlers.ts
@@ -481,15 +481,15 @@ export const handleRetrievePackage = async (id: string, headers: { [key: string]
 // Handler for /package/{id} - PUT (Update Package)
 export const handleUpdatePackage = async (id: string, body: string, headers: { [key: string]: string | undefined }): Promise<APIGatewayProxyResult> => {
   // Authenticate the request
-  // let user: AuthenticatedUser;
-  // try {
-  //   user = await authenticate(headers);
-  // } catch (err: any) {
-  //   return sendResponse(err.statusCode, { message: err.message });
-  // }
-  // if (!user.permissions.includes('upload')) {
-  //   return sendResponse(403, { message: 'You do not have permission to update packages.' });
-  // }
+  let user: AuthenticatedUser;
+  try {
+    user = await authenticate(headers);
+  } catch (err: any) {
+    return sendResponse(err.statusCode, { message: err.message });
+  }
+  if (!user.permissions.includes('upload')) {
+    return sendResponse(403, { message: 'You do not have permission to update packages.' });
+  }
 
   const updatedPackage: Package = JSON.parse(body);
   const { metadata, data } = updatedPackage;
